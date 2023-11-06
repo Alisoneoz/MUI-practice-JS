7d5dd510d56084c386fb98fdf65ac3b4183f52eb
alisoneoz <alisonestephanyoz@gmail.com>
2023-11-06 17:52:27 -0400

    Add MUI components to test, and add code to create theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
-import { Container } from "@mui/material";
+import { Box, Container, Typography } from "@mui/material";
 
 function App() {
   
 
   return (
     
- <Container>Hello World!</Container> 
+ <Container sx={{bgcolor:'tomato', height: '100vh'}}>
+  <Typography variant="h1">Hello World!</Typography>
+ <Box >
+  <Typography variant="h2"> This is text inside a Box component</Typography>
+ </Box>
+  
+  
+  </Container> 
   )
 }
 
